2c12e2eb943e1cf007ba51d4ef5a0c6e02bbfba8
Antoine Richard <antoiner@nvidia.com>
2025-08-28 13:58:20 +0200

    WIP

--- source/isaaclab/isaaclab/fluid_physics/fluid/fluid_cfg.py
+++ source/isaaclab/isaaclab/fluid_physics/fluid/fluid_cfg.py
@@ -1,40 +1,4 @@
-REFERENCE_AIR_DENSITY = {
-    0: 1.2250,
-    1000: 1.1116,
-    2000: 1.0065,
-    3000: 0.9091,
-    4000: 0.8191,
-    5000: 0.7361,
-    6000: 0.6557,
-    7000: 0.5895,
-    8000: 0.5252,
-    9000: 0.4664,
-    10000: 0.4127,
-    11000: 0.3639,
-    13000: 0.2655,
-    15000: 0.1937,
-    17000: 0.1423,
-    20000: 0.0880,
-    25000: 0.0395,
-    30000: 0.0180,
-    32000: 0.0132,
-    35000: 0.0082,
-    40000: 0.0039,
-    45000: 0.0019,
-    47000: 0.0014,
-    50000: 0.0010,
-    60000: 0.000288,
-    70000: 0.000086,
-    80000: 0.000015,
-    90000: 0.000003,
-    100000: 0.0000005,
-    110000: 0.0000001,
-}
-
-REFERENCE_WATER_DENSITY = {
-    "salt_water": 1025,
-    "fresh_water": 1000,
-}
+from .references import REFERENCE_AIR_DENSITY, REFERENCE_WATER_DENSITY, REFERENCE_WATER_VISCOSITY, REFERENCE_AIR_VISCOSITY
 
 class BaseFluidCfg:
     def __init__(self):
@@ -42,9 +6,9 @@ class BaseFluidCfg:
 
 class IncompressibleFluidCfg(BaseFluidCfg):
     def __init__(self):
+        super().__init__()
         self.density = REFERENCE_WATER_DENSITY["fresh_water"]
-        self.viscosity = 0.001
-        self.current = None
+        self.viscosity = REFERENCE_WATER_VISCOSITY["fresh_water"]
 
 class AtmosphericFluidCfg(BaseFluidCfg):
     def __init__(self):
@@ -52,5 +16,4 @@ class AtmosphericFluidCfg(BaseFluidCfg):
         self.reference_density_values = REFERENCE_AIR_DENSITY.values()
         self.reference_density_heights = REFERENCE_AIR_DENSITY.keys()
         self.initial_altitude = 0
-        self.viscosity = 0.0000181
-        self.current = None
+        self.viscosity = REFERENCE_AIR_VISCOSITY["20C"]
